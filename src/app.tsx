55cbc05d710142633c37e18d2de12c89f3efbf5a
gosha <goshan7890@yandex.ru>
2021-04-25 14:37:09 +0300

    add 3d model view

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,27 @@
 import React, { useEffect } from 'react';
 import ReactDom from 'react-dom';
-import ModelViewer from 'react-model-viewer';
+import { OBJModel } from 'react-3d-viewer'
+import model from '../assets/uploads_files_1916312_tulips_alutaroma_free.obj';
 import { Service } from './service';
 import { Button } from 'react-bootstrap';
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
-const modelPath = './assets/ffbe3891-e426-42ff-903e-10d95b773783.gltf';
+const modelPath = '../assets/uploads_files_1916312_tulips_alutaroma_free.obj';
 
 const App = () => {
-    const service = new Service();
-    useEffect(() => {
-        service.getQuote().then(console.log)
-    }, [])
-    const click = () => {
-        service.getQuote().then(console.log)
-    }
+    // const service = new Service();
+    // useEffect(() => {
+    //     service.getQuote().then(console.log)
+    // }, [])
+    // const click = () => {
+    //     service.getQuote().then(console.log)
+    // }
     return (
         <h1>
-            Hi from a react app
-            <Button onClick={click}>111</Button>
-            {/* <ModelViewer type="gtlf" src={modelPath} /> */}
+            {/* Hi from a react app
+            <Button onClick={click}>111</Button> */}
+            <OBJModel src={model} texPath=""/>
         </h1>
     )
 }
